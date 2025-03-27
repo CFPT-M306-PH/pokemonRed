a52f9f17ac6728f0d9171122e642f47f5b8da61c
hugo-mrmdc <hugo.mermoudclairbaux@gmail.com>
2025-03-27 15:58:43 +0100

    commit origne

--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -1,31 +1,48 @@
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 5f; // Vitesse du joueur
+	private Animator animator;
 	private Rigidbody2D rb;
-	private Vector2 movement;
+	public float speed = 5f;
+	private Vector2 moveInput;
 
 	void Start()
 	{
+		animator = GetComponent<Animator>();
 		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void Update()
 	{
-		// R�cup�re l'entr�e du clavier (ZQSD ou Fl�ches)
-		movement.x = Input.GetAxisRaw("Horizontal");
-		movement.y = Input.GetAxisRaw("Vertical");
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
 
-		// Normalisation pour �viter d'aller plus vite en diagonale
-		movement = movement.normalized;
+		// Empêcher le mouvement diagonal : Priorité au mouvement vertical
+		if (moveY != 0)
+		{
+			moveX = 0;
+		}
+
+		moveInput = new Vector2(moveX, moveY).normalized;
+
+		// Appliquer le mouvement
+		rb.linearVelocity = moveInput * speed;
+
+		// Désactiver toutes les animations
+		animator.SetBool("isWalkingDown", false);
+		animator.SetBool("isWalkingUp", false);
+		animator.SetBool("isWalkingLeft", false);
+		animator.SetBool("isWalkingRight", false);
+
+		// Activer l'animation correspondante
+		if (moveY < 0)
+			animator.SetBool("isWalkingDown", true);
+		else if (moveY > 0)
+			animator.SetBool("isWalkingUp", true);
+		else if (moveX < 0)
+			animator.SetBool("isWalkingLeft", true);
+		else if (moveX > 0)
+			animator.SetBool("isWalkingRight", true);
 	}
-
-	void FixedUpdate()
-	{
-		// D�placement du joueur
-		rb.linearVelocity = movement * speed;
-	}
-
-
 }
